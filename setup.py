006f93a4c1996656102a1e2695803a1e7c1e7d10
Eugen Ciur <eugen@papermerge.com>
2021-11-22 22:06:03 +0100

    versions bump

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
+#
+# jinja2 == 3.0.3
+# ocrmypdf == 12.0.3
+# lxml == 4.6.2
+#
 
 setup(
     packages=find_packages(),
     install_requires=[
-        "ocrmypdf == 12.0.3",
+        "ocrmypdf == 12.7.2",
+        "Jinja2 == 3.0.3",
+        "lxml == 4.6.2"
     ],
     python_requires='>=3.7',
 )
